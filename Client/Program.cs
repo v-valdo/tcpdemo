f659736c1f005e9a6e8fab06aa2e71b55a7e00c6
v-valdo <v.sersam@gmail.com>
2024-01-19 15:10:23 +0100

    final

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+IPHostEntry host = await Dns.GetHostEntryAsync(Dns.GetHostName());
+IPAddress address = host.AddressList[0];
+IPEndPoint endpoint = new(address, 3000);
+
+using TcpClient client = new();
+await client.ConnectAsync(endpoint);
+
+await using NetworkStream stream = client.GetStream();
+
+byte[] buffer = new byte[1_024];
+int received = await stream.ReadAsync(buffer);
+
+string message = Encoding.UTF8.GetString(buffer, 0, received);
+Console.WriteLine(message);
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+IPHostEntry host = await Dns.GetHostEntryAsync(Dns.GetHostName());
+IPAddress address = host.AddressList[0];
+IPEndPoint endpoint = new(address, 3000);
+
+TcpListener listener = new(endpoint);
+
+try
+{
+    listener.Start();
+    Console.WriteLine();
+    using TcpClient handler = await listener.AcceptTcpClientAsync();
+    await using NetworkStream stream = handler.GetStream();
+
+    string message = $"Connection Established";
+    byte[] bytes = Encoding.UTF8.GetBytes(message);
+
+    await stream.WriteAsync(bytes);
+}
+
+finally
+{
+    listener.Stop();
+}
